95b97aabd226be508da1a26d323f265e3b3cf8a1
Brian Holt <brian.c.holt@gmail.com>
2014-03-12 22:48:49 -0700

    new data and script for mooc

--- mitRegression.r
+++ mitRegression.r
@@ -1,4 +1,6 @@
-setwd("I:\\My Data Sources\\mooc")
+getwd()
+home<-getwd()
+setwd(paste0(home, "/MitAnal"))
 wine<-read.csv("wine.csv")
 str(wine)
 summary(wine)
@@ -15,6 +17,27 @@ model3<-lm(wine$Price~AGST+HarvestRain+WinterRain+
 summary(model3)
 SSE<-sum(model3$residuals^2)
 
-model4<-lm(wine$Price~HarvestRain+WinterRain,data=wine)
+model5<-lm(wine$Price~HarvestRain+WinterRain,data=wine)
+summary(model5)
+SSE<-sum(model5$residuals^2)
+SSE
+model4<-lm(wine$Price~AGST+HarvestRain+WinterRain+
+             Age,data=wine)
 summary(model4)
 SSE<-sum(model4$residuals^2)
+SSE
+cor(wine$WinterRain,wine$Price)
+cor(wine$Age,wine$FrancePop)
+cor(wine)
+
+
+wine_test<-read.csv("wine_test.csv")
+str(wine_test)
+predictTest<-predict(model4,newdata=wine_test)
+predictTest
+class(predictTest)
+
+SSE=sum((wine_test$Price - predictTest)^2) 
+SST =sum((wine_test$Price - mean(wine$Price))^2)  #the mean comes from the baseline model
+
+1-SSE/SST
