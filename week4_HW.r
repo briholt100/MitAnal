40b3f64a1a26c53ea1ddab61ab95a4d3dbf475d4
Brian Holt <brian.c.holt@gmail.com>
2014-04-05 22:40:29 -0700

    new data and script for HW 4

---
+++ week4_HW.r
@@ -0,0 +1,10 @@
+()
+school<-"I:/My Data Sources/mooc/MitAnalytic"
+setwd(school)
+home<-getwd()
+setwd(paste0(home, "/mooc/MitAnal"))
+dater<-getwd()
+setwd(paste0(dater, "/MitAnal"))
+dir()
+gerber = read.csv("./data/gerber.csv")
+str(gerber)
