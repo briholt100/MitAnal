3186b0dbbf2f07c6e6f932c02af62b3f91db32c9
Brian Holt <brian.c.holt@gmail.com>
2014-04-12 13:35:15 -0700

    new script week 5 hw

--- Week5_HW.R
+++ Week5_HW.R
@@ -10,3 +10,57 @@ dir()
 wiki<-read.csv("data/wiki.csv",stringsAsFactors=FALSE)
 wiki$Vandal = as.factor(wiki$Vandal)
 str(wiki)
+table(wiki$Vandal)
+
+
+corpusAdded = Corpus(VectorSource(wiki$Added))
+
+corpusAdded[[1]]
+
+
+# Pre-process data
+#corpusAdded <- tm_map(corpusAdded, tolower)
+
+#corpusAdded <- tm_map(corpusAdded, removePunctuation)
+
+corpusAdded <- tm_map(corpusAdded, removeWords, stopwords("english"))
+
+corpusAdded <- tm_map(corpusAdded, stemDocument)
+
+# Look at first 
+corpusAdded[[1]]
+
+dtmAdded<-DocumentTermMatrix(corpusAdded)
+dtmAdded
+
+sparseAdded<-removeSparseTerms(dtmAdded, 0.997)
+
+wordsAdded<-as.data.frame(as.matrix(sparseAdded))
+colnames(wordsAdded) = paste("A", colnames(wordsAdded))
+
+###########
+corpusRemoved = Corpus(VectorSource(wiki$Removed))
+
+corpusRemoved[[1]]
+
+
+# Pre-process data
+#corpusRemoved <- tm_map(corpusRemoved, tolower)
+
+#corpusRemoved <- tm_map(corpusRemoved, removePunctuation)
+
+corpusRemoved <- tm_map(corpusRemoved, removeWords, stopwords("english"))
+
+corpusRemoved <- tm_map(corpusRemoved, stemDocument)
+
+# Look at first 
+corpusRemoved[[3]]
+
+dtmRemoved<-DocumentTermMatrix(corpusRemoved)
+dtmRemoved
+
+sparseRemoved<-removeSparseTerms(dtmRemoved, 0.997)
+
+wordsRemoved<-as.data.frame(as.matrix(sparseRemoved))
+colnames(wordsRemoved) = paste("R", colnames(wordsRemoved))
+str(wordsRemoved)
