cf0676a390972623c1eef5fdb32492bab0ce7af3
Brian Holt <brian.c.holt@gmail.com>
2014-04-17 19:40:33 -0700

    added script to kaggle test

--- kaggle.R
+++ kaggle.R
@@ -5,3 +5,9 @@ train<-read.csv("./data/Kaggle_train.csv")
 test<-read.csv("./data/Kaggle_test.csv")
 str(train)
 table(train$Happy,train$Income)
+
+HappyLog<-glm(Happy~.,data=train,family="binomial")
+summary (HappyLog)
+
+boxplot(train$YOB~train$Gender)
+?boxplot
