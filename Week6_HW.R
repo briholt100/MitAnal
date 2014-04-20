cbfec5b67c434f5d1b5556d346ba0e88150a260d
Brian Holt <brian.c.holt@gmail.com>
2014-04-20 14:51:14 -0700

    new data, changed script to HW6

--- Week6_HW.R
+++ Week6_HW.R
@@ -73,11 +73,57 @@ for (i in 1:7){print(nrow(kosClust[[i]]))}
 for (i in 1:7){
   print (i)
   print(tail(sort(colMeans(kosClust[[i]]))))
-  
 }
-
 
 tableOut<-table(kosClusters, KMC$cluster) #kosClusters was derrived from heirarchical found way up above.  KMC is a k-means
 margin.table(tableOut, c(1,2))
 
 ftable(kosClusters, KMC$cluster)
+
+
+##New section HW #6
+
+airlines<-read.csv("./data/AirlinesCluster.csv")
+str(airlines)
+summary(airlines)
+install.packages("caret")
+library(caret)
+preproc = preProcess(airlines)
+airlinesNorm = predict(preproc, airlines)
+summary(airlinesNorm)
+
+#hclust
+
+distance<-dist(airlinesNorm,method="euclidian")
+hierClust<-hclust(distance,method="ward.D")
+plot(hierClust)
+rect.hclust(hierClust, k=5 , border = "blue")
+str(airlines)
+airClust<-cutree(hierClust,k=5)
+table(airClust)
+tapply(airlines$Balance, airClust, mean)
+tapply(airlines$QualMiles, airClust, mean)
+tapply(airlines$BonusMiles, airClust, mean)
+tapply(airlines$BonusTrans, airClust, mean)
+tapply(airlines$FlightMiles, airClust, mean)
+tapply(airlines$FlightTrans, airClust, mean)
+tapply(airlines$DaysSinceEnroll, airClust, mean)
+lapply(split(airlines, airClust), colMeans) #alternative way of getting at same data
+
+
+# Specify number of clusters
+k = 5
+
+# Run k-means
+set.seed(88)
+KMC = kmeans(airlinesNorm, centers = k, iter.max = 1000)
+str(KMC)
+objects(KMC)
+table(KMC$cluster)
+
+# Extract clusters
+AirKclusters = KMC$cluster  #associated cluster with observations.  length should equal data nrow
+KMC$centers
+lapply(split(airlines, KMC$cluster), colMeans) #shows the cluster averages by non-normed data
+
+
