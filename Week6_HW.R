96e8844ab6a86a643e5195088d31c6495d96c593
Brian Holt <brian.c.holt@gmail.com>
2014-04-19 22:40:57 -0700

    added to  HW6

--- Week6_HW.R
+++ Week6_HW.R
@@ -16,7 +16,63 @@ kosVector2 = as.vector(kos)
 str(kosVector2)
 
 # Compute distances
-distance = dist(kosVector, method = "euclidean")
+distance = dist(kosMatrix[,-1], method = "euclidean")
 
 # Hierarchical clustering
 clusterIntensity = hclust(distance, method="ward.D")
+plot(clusterIntensity)
+
+k=7
+
+rect.hclust(clusterIntensity, k , border = "blue")
+kosClusters = cutree(clusterIntensity, k)
+head(kosClusters)
+
+tapply(kosMatrix,kosClusters,mean)
+table(kosClusters)
+
+kosClust1<-subset(kos,kosClusters == 1)
+kosClust2<-subset(kos,kosClusters == 2)
+kosClust3<-subset(kos,kosClusters == 3)
+kosClust4<-subset(kos,kosClusters == 4)
+kosClust5<-subset(kos,kosClusters == 5)
+kosClust6<-subset(kos,kosClusters == 6)
+kosClust7<-subset(kos,kosClusters == 7)
+
+
+#or do this: kosClust = split(kos, kosClusters)   ##cluster 1 can be accessed HierCluster[[1]], cluster 2 HierCluster[[2]]
+tail(sort(colMeans(kosClust1[-1])))
+tail(sort(colMeans(kosClust2[-1])))
+tail(sort(colMeans(kosClust3[-1])))
+tail(sort(colMeans(kosClust4[-1])))
+tail(sort(colMeans(kosClust5[-1])))
+tail(sort(colMeans(kosClust6[-1])))
+tail(sort(colMeans(kosClust7[-1])))
+
+
+
+##2.1
+
+k = 7
+
+# Run k-means
+set.seed(1000)
+KMC = kmeans(kos[,-1], centers = k, 
+             #iter.max = 1000
+             )
+str(KMC)
+
+# Extract clusters
+kosKclusters = KMC$cluster
+KMC$centers[1]
+
+kosClust = split(kos[,-1],kosKclusters)
+for (i in 1:7){print(nrow(kosClust[[i]]))}
+
+
+for (i in 1:7){
+  print (i)
+  print(tail(sort(colMeans(kosClust[[i]]))))
+  
+}
+
