649a3a3f215be4829b0a3cc9471ca1fd1d282b23
briholt100 <brian.c.holt@gmail.com>
2014-05-16 16:28:18 -0700

    nwe final script and data

--- final/final.R
+++ final/final.R
@@ -1,8 +1,9 @@
 # Final for MitAnal
-
+getwd()
 elantra<-read.csv("elantra.csv")
 str(elantra)
 summary(elantra)
+elantra$factor.month<-as.factor(elantra$Month)
 
 # Subset data into training set and test set
 train = subset(elantra, Year < 2013)
@@ -20,3 +21,64 @@ summary(sales.lm1)
 
 sales.lm2 <- lm(ElantraSales~ Month + Unemployment + CPI_all + CPI_energy + Queries, data = train)
 summary(sales.lm2)
+
+table(elantra$Month)
+#jan times month factor:
+abs(1*110.69 - 5*110.69)
+
+
+#use month as factor.
+
+sales.lm3 <- lm(ElantraSales~ factor.month + Unemployment + CPI_all + CPI_energy + Queries, data = train)
+summary(sales.lm3)
+
+#correlations:
+cor(elantra[,1:7])
+
+
+step(sales.lm3)
+
+
+sales.lm3 <- lm(ElantraSales~ factor.month + Unemployment + CPI_all + CPI_energy, data = train)
+
+SSE<-sum(sales.lm3$residuals^2)  #general
+
+predict.test<-predict(sales.lm3,newdata=test)
+SSE=sum((test$ElantraSales - predict.test)^2) 
+
+mean(train$ElantraSales)
+
+SST<-sum((mean(train$ElantraSales)-test$ElantraSales)^2)
+1-SSE/SST   #R^2
+
+
+#what is the largest absolute error that we make in our test set prediction?  
+
+which.max(sort(abs(test$ElantraSales - predict.test)))
+table(test$factor.month)
+
+sort(abs(test$ElantraSales[test$factor.month==3] - predict.test[test$factor.month==3]))
+test[5,]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ final/final2.R
@@ -0,0 +1,38 @@
+# Final part 2 for MitAnal
+getwd()
+nytimes<-read.csv("nytimes.csv",stringsAsFactors=FALSE)
+str(nytimes)
+table(nytimes$popular)
+105/nrow(nytimes)
+
+cor(nchar(nytimes$headline),nytimes$popular)
+
+nytimes$popular<-as.factor(nytimes$popular)
+nytimes$type<-as.factor(nytimes$type)
+
+library(caTools)
+set.seed(144)
+spl<-sample.split(nytimes$popular,SplitRatio = .7)
+spl
+train<-subset(nytimes,spl==T)
+test<-subset(nytimes,spl==F)
+
+
+#log model
+
+pop.glm1<-glm(popular~print+type+word.count,data=train,family="binomial")
+summary(pop.glm1)
+
+
+trial<-data.frame(1,"News",682)
+colnames(trial)<-c("print","type","word.count")
+trial
+predict(pop.glm1,newdata=trial,type="response")
+pop.glm1$coef
+-2.5075573108 + (-0.8468333174) + 0.9055929357 +  0.0002599972 * 682
+=-2.27148
+1/(1+exp(-(-2.27148)))
+#the log response function:   1/1 +e^-(-2.27)
+
+
+exp(-0.8468333)
