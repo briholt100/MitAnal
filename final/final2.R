62143fe4b6b778844a8af55943bf30ac445c8916
Brian Holt <brian.c.holt@gmail.com>
2014-05-17 00:08:15 -0700

    slight script changes to final2

--- final/final2.R
+++ final/final2.R
@@ -29,8 +29,8 @@ colnames(trial)<-c("print","type","word.count")
 trial
 predict(pop.glm1,newdata=trial,type="response")
 pop.glm1$coef
--2.5075573108 + (-0.8468333174) + 0.9055929357 +  0.0002599972 * 682
-=-2.27148
+-2.5075573108 + (-0.8468333174) + 0.9055929357 +  0.0002599972 * 682  #=-2.27148
+
 1/(1+exp(-(-2.27148)))
 #the log response function:   1/1 +e^-(-2.27)
 
@@ -38,3 +38,19 @@ exp(-0.8468333)  #beta coef are the log odds for that variable.  By taking e^bet
 
 
 ##test set predictions next
+
+predictTest<-predict(pop.glm1,newdata=test,type="response")
+table(test$popular,predictTest>.5)
+
+
+library(ROCR)
+ROCRpred<-prediction(predictTest,test$popular)
+ROCRperf<-performance(ROCRpred, "tpr","fpr")
+plot(ROCRperf,colorize =T, print.cutoffs.at=seq(0,1,0.1),text.adj=c(-0.2,1.7),)
+auc = as.numeric(performance(ROCRpred, "auc")@y.values)
+
+
+predictTest = predict(QualityLog, type="response", newdata=qualityTest)
+ROCRpredTest = prediction(predictTest, qualityTest$PoorCare)
+
+auc = as.numeric(performance(ROCRpredTest, "auc")@y.values)
